19f43631d41e73de0b513750ae3aed95d18d0595
takaira <c0a2413397@edu.teu.ac.jp>
2025-11-18 17:11:35 +0900

    練習9残像解消

--- pygame_image.py
+++ pygame_image.py
@@ -11,17 +11,16 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = tmr %3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
